bfc24c9c8fa283756051c836e01621cf961a7078
nelson t. aoki <nelson.aoki@gmail.com>
2024-08-13 12:22:11 -0300

    mensagem especial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O mundo não dá voltas, capota...</h1>
+    return <h1><p>Feliz aniversário, Gisele. Muita saúde, paz, felicidade...</p><p>E $$$ também, se for possível ;)</p><p>O mundo não dá voltas, capota...</p></h1>
 }
 
 export default Home;
